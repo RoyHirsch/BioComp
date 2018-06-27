b70e6559db6475311d9e3932cf46a79b3a5f2a7b
RoyHirsch <roymhirsch@gmail.com>
2018-06-27 16:36:32 +0300

    Better documentation of DataPipeline

--- src/Utils/read_data.py
+++ src/Utils/read_data.py
@@ -3,9 +3,6 @@ simple function to read and pre-process the data
 '''
 
 import os
-import re
-import pandas
-import numpy as np
 import time
 import keras
 from keras.utils import to_categorical
@@ -13,27 +10,41 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import SGD
-from keras.regularizers import *
 
+########################
+# CONSTANTS
+########################
 SELEX_LEN = 20
 PBM_LEN_TOTAL = 60
 PBM_LEN = 36
 
+
+#########################################################################
+# Description: This class manages the data loading to the model.
+# 			   If scans dataRoot for all the different files and will generate
+# 	           the relevant train and test data per experiment.
+# Input: dataRoot - root folder that holds all the files.
+#        mode - debug for loading only experiment #1 , operation - real-world operation.
+#        argsDist (optional)
+#                trainPercentage - num in [0,1]
+#                isPadding       - bool
+# Output: DataPipeline obj
+#########################################################################
 class DataPipeline(object):
 
-	# argsDist {trainPercentage: num, isPadding: bool}
-	def __init__(self, dataRoot='', mode='Train', argsDist={}):
+	def __init__(self, dataRoot='', mode='debug', argsDist={}):
 
 		print('+++++++++ DataPipeline was created +++++++++')
 		self.dataRoot = dataRoot
 		self.mode = mode
 		self.argsDict = argsDist
 		self.extract_samples_list()
-		self.currentSample = self.get_sample_number(mode='debug')
-		self.trainData, self.validationData, self.trainLabel, self.validationLabel, self.testData = self.get_data_and_labels_for_sample_number()
+		self.currentSample = self.get_sample_number()
+		self.trainData, self.validationData, self.trainLabel, self.validationLabel,\
+		self.testData = self.get_data_and_labels_for_sample_number()
 
 	#########################################################################
-	# Description: The function reads all the files in a root folder
+	# Description: The function reads all the files in a root folder.
 	# Input: trainDataRoot - path to the folder that contains the files
 	# Output: filenamesList, numberOfSamples
 	#########################################################################
@@ -49,21 +60,38 @@ class DataPipeline(object):
 		maxSampleNumber = max(sampleNumbersList)
 		minSampleNumber = min(sampleNumbersList)
 
-		# TODO:
 		assert (self.numberOfSamples == maxSampleNumber-minSampleNumber+1), "Num of samples is not ,matched"
 
 		return
 
-	def get_sample_number(self, mode):
-		if mode == 'debug':
+	#########################################################################
+	# Description: Gets the current experiment sample to work on.
+	# Input:
+	# Output: experiment number
+	#########################################################################
+	def get_sample_number(self):
+		if self.mode == 'debug':
 			return 1
-		elif mode == 'random':
+
+		elif self.mode == 'random':
 			return int(np.random.randint(min(self.numberOfSamples), max(self.numberOfSamples), 1))
+
+		elif self.mode == 'operation':
+			# TODO : finish operaion mode
+			pass
+
 		else:
 			raise ValueError('No such a mode {}'.format(str(mode)))
 
+	#########################################################################
+	# Description: Given an experiment number the function reads the SELEX and PBM data.
+	#              It extracts the data and pre-process it if necessary.
+	# Input:
+	# Output: trainData, validationData, trainLabel, validationLabel, testData
+	#########################################################################
 	def get_data_and_labels_for_sample_number(self):
 		currTime = time.time()
+
 		# Read PBM test file:
 		pbmFile = ''
 		testFileName = 'TF{}_pbm.txt'.format(self.currentSample)
@@ -84,8 +112,11 @@ class DataPipeline(object):
 				break
 		selexFiles = np.array(selexFiles)
 		print('Loaded PBM and SELEX files for sample number: {}.\nNumber of selex files: {}.'.format(self.currentSample, i))
+
 		endTime = time.time()
 		print('Loading the data took {} seconds.'.format(round(endTime-currTime, 2)))
+
+		# Pre-process the data:
 		trainData, validationData, trainLabel, validationLabel = self.process_selex_data(selexFiles)
 		testData = self.process_PBM_data(pbmFile)
 
@@ -93,9 +124,8 @@ class DataPipeline(object):
 
 	#########################################################################
 	# Description: Naive implementation for extracting SELEX data.
-	#              Cycle '0' will be labeled as negative and the other cycles as positive
-	#
-	#              Basically the function generates the train and validation data
+	#              Cycle '0' will be labeled as negative and the other cycles as positive.
+	#              Basically the function generates the train and validation data.
 	# Input: selexsFilesList
 	# Output: trainData, validationData, trainLabel, validationLabel
 	#########################################################################
@@ -123,7 +153,6 @@ class DataPipeline(object):
 		union = np.random.permutation(union)
 		data, label = np.split(union, 2, axis=1)
 
-
 		# Divide into train and validation datasets
 		trainPercentage = self.argsDict.pop('trainPercentage', 0.7)
 		slice = round(trainPercentage*data.shape[0])
@@ -136,23 +165,6 @@ class DataPipeline(object):
 	# placeholder function
 	def process_PBM_data(self, pbmData):
 		return map(oneHot, pbmData)
-
-	# Creates a random permutation vector of ind in size of train data
-	def init_batching_for_epoch(self):
-		self.permutatedInd = np.random.permutation(np.shape(self.trainData)[0])
-		self.batchStartInd = 0
-		return
-
-	# Extracts a randomly selected batch for train step
-	# output two numpy array one for samples and one for labels
-	def next_batch_from_epoch(self):
-		batchSize = self.argsDict.pop('batchSize', 16)
-		# TODO - extract only the strigs without the count column
-		batchSamples = self.trainData[self.permutatedInd[self.batchStartInd: self.batchStartInd + batchSize], 0]
-		batchLabels = self.trainData[self.permutatedInd[self.batchStartInd: self.batchStartInd + batchSize], 2]
-		self.batchStartInd += batchSize
-
-		return batchSamples, batchLabels
 
 	@staticmethod
 	def read_pbm_file(pbmFilePath):
@@ -171,66 +183,13 @@ class DataPipeline(object):
 		splitedText = [row.split('\t') for row in text]
 		return splitedText[:-1]
 
-########################
-# HELPER FUNCTIONS 
-########################
-# def convert_dna_string_to_matrix(dnaString):
-# 	matrix = list()
-# 	for base in dnaString:
-# 		if base == 'A':
-# 			base_encoding = [1, 0, 0, 0]
-# 		elif base == 'C':
-# 			base_encoding = [0, 1, 0, 0]
-# 		elif base == 'G':
-# 			base_encoding = [0, 0, 1, 0]
-# 		elif base == 'T':
-# 			base_encoding = [0, 0, 0, 1]
-# 		else:
-# 			raise ValueError
-# 		matrix.append(base_encoding)
-# 	yield np.array(matrix).T
-
-'''
-Input matrix:             Output matrix: padding of 2 columns each side
-								
-[[0, 0, 0, 1, 0, 1],     [[0,0, 0, 0, 0, 1, 0, 1,0,0],
- [1, 0, 0, 0, 1, 0],      [0,0, 1, 0, 0, 0, 1, 0,0,0], 
- [0, 0, 1, 0, 0, 0],      [0,0, 0, 0, 1, 0, 0, 0,0,0],
- [0, 1, 0, 0, 0, 0]]      [0,0, 0, 1, 0, 0, 0, 0,0,0]]
-'''
-# # maxSize - the max number of columns (represents char)
-# def vertical_zero_pad_matrix(matrix, maxSize):
-# 	H, W = np.shape(matrix)
-# 	colNum = (maxSize - W) // 2
-# 	assert (colNum == (maxSize - W) / 2), 'Invalid size of one-hot matrix of DNA sequence'
-#
-# 	paddingMatrix = np.zeros((H, colNum))
-# 	return np.concatenate((paddingMatrix, matrix, paddingMatrix), axis=1)
-
-def oneHot(string):
-	trantab = str.maketrans('ACGT','0123')
-	string = string[0] + 'ACGT'
-	data = list(string.translate(trantab))
-	return to_categorical(data)[:-4]
-
-def oneHotZeroPad(string, maxSize=PBM_LEN):
-	trantab = str.maketrans('ACGT','0123')
-	string = string[0] + 'ACGT'
-	data = list(string.translate(trantab))
-
-	# Convert to one-hot matrix: Lx4
-	matrix = to_categorical(data)[:-4]
-
-	# Zero pad to maxSizex4
-	length = len(string) - 4
-	pad = (maxSize - length) // 2
-	if ((maxSize - length) // 2 != (maxSize - length) / 2):
-		raise ValueError ('Cannot zero pad the string')
-	padMatrix = np.zeros([pad, 4])
-	return np.concatenate((padMatrix, matrix, padMatrix), axis=0)
-
+#########################################################################
+# Description: Creates 'Generator' object that holds the date pipe for train/val data.
+# Input:
+# Output: DataGenerator obj
+#########################################################################
 class DataGenerator(keras.utils.Sequence):
-	def __init__(self, data, label, batch_size=32, dim=(36, 4), n_channels=1,n_classes=1, shuffle=True):
+	def __init__(self, data, label, batch_size=32, dim=(36, 4), n_channels=1, n_classes=1, shuffle=True):
 		self.dim = dim
 		self.batch_size = batch_size
 		self.label = label
@@ -241,18 +200,21 @@ class DataGenerator(keras.utils.Sequence):
 		self.on_epoch_end()
 
 	def __len__(self):
-		'Denotes the number of batches per epoch'
+		# Denotes the number of batches per epoch
 		return int(np.floor(len(self.data) / self.batch_size))
 
 	def on_epoch_end(self):
-		'Updates indexes after each epoch'
+		# Updates indexes after each epoch
 		self.indexes = np.arange(len(self.data))
 		if self.shuffle == True:
 			np.random.shuffle(self.indexes)
 
+	#########################################################################
+	# Description: Generate one batch of data, evalutes and pre-process the data.
+	# Input:
+	# Output: DataGenerator obj
+	#########################################################################
 	def __getitem__(self, index):
-		'Generate one batch of data'
-		# Generate indexes of the batch
 		indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
 		batchData = [self.data[k] for k in indexes]
@@ -261,15 +223,73 @@ class DataGenerator(keras.utils.Sequence):
 		batchLabel = np.array([self.label[k] for k in indexes]).reshape(self.batch_size, self.n_classes)
 		return batchData, batchLabel
 
+#########################################################################
+# Description: Creates 'Generator' object that holds the date pipe for test data (PBM file).
+# Input:
+# Output: TestDataGenerator obj
+#########################################################################
+class TestDataGenerator(keras.utils.Sequence):
+	def __init__(self, data, batch_size=256, dim=(36, 4), n_channels=1, n_classes=1, shuffle=True):
+		self.dim = dim
+		self.batch_size = batch_size
+		self.data = data
+		self.n_channels = n_channels
+		self.n_classes = n_classes
+		self.shuffle = shuffle
+		self.on_epoch_end()
+
+	def __len__(self):
+		# Denotes the number of batches per epoch
+		return int(np.floor(len(self.data) / self.batch_size))
+
+	#########################################################################
+	# Description: Generate one batch of data, evaluates and pre-process the data.
+	# Input:
+	# Output: DataGenerator obj
+	#########################################################################
+	def __getitem__(self, index):
+		indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+		batchData = [self.data[k] for k in indexes]
+		batchData = list(map(oneHot, batchData))
+		batchData = np.stack(batchData, axis=0)
+		return batchData
+
+########################
+# HELPER FUNCTIONS
+########################
+def oneHot(string):
+	trantab = str.maketrans('ACGT', '0123')
+	string = string[0] + 'ACGT'
+	data = list(string.translate(trantab))
+	return to_categorical(data)[:-4]
+
+def oneHotZeroPad(string, maxSize=PBM_LEN):
+	trantab = str.maketrans('ACGT', '0123')
+	string = string[0] + 'ACGT'
+	data = list(string.translate(trantab))
+
+	# Convert to one-hot matrix: Lx4
+	matrix = to_categorical(data)[:-4]
+
+	# Zero pad to maxSizex4
+	length = len(string) - 4
+	pad = (maxSize - length) // 2
+	if ((maxSize - length) // 2 != (maxSize - length) / 2):
+		raise ValueError('Cannot zero pad the string')
+	padMatrix = np.zeros([pad, 4])
+	return np.concatenate((padMatrix, matrix, padMatrix), axis=0)
 
 ########################
 # DEBUG
+# Simple model to illustrate the dataPipe behavior
 ########################
 debugPath = '/Users/royhirsch/Documents/GitHub/BioComp/train_data/'
 dataObj = DataPipeline(dataRoot=debugPath, mode='Train', argsDist={})
 
 training_generator = DataGenerator(dataObj.trainData, dataObj.trainLabel)
 validation_generator = DataGenerator(dataObj.validationData, dataObj.validationLabel)
+test_generator = TestDataGenerator(dataObj.testData)
 
 model=Sequential()
 model.add(Conv1D(filters=4, kernel_size=3, strides=1, kernel_initializer='RandomNormal', activation='relu',
@@ -285,6 +305,6 @@ model.fit_generator(generator=training_generator,
                     use_multiprocessing=True,
                     workers=6,
                     verbose=1)
-# dnaString = ['ACAAGTTATG']
-# mactrixG = oneHotZeroPad(dnaString)
 
+ס   # returns np array of predictions
+predictions = model.predict_generator(generator=test_generator)
